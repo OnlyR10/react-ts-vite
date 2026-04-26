ec2e2b8ec1937ff50991f4ace70ebcf0f61a2d86
Kirill Naydovich <kirillnaydovich@gmail.com>
2026-04-27 01:16:03 +0300

    Изменил проверку расширения файлов при импорте, убрал дублирование алиасов, перенес настройки команд в конфигурацию

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-import { resolve } from "node:path";
-
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
@@ -7,28 +5,34 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
-      routesDirectory: "./src/app/router/routes",
-      generatedRouteTree: "./src/app/router/routeTree.gen.ts",
-      enableRouteTreeFormatting: true, // ! значение по умолчанию. Включает функцию форматирования сгенерированного файла дерева маршрутов, что может занять много времени при работе над крупными проектами
-    }),
-    react(),
-    tailwindcss(),
-    svgr(),
-  ],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-      "@app": resolve(__dirname, "src/app"),
-      "@pages": resolve(__dirname, "src/pages"),
-      "@widgets": resolve(__dirname, "src/widgets"),
-      "@entities": resolve(__dirname, "src/entities"),
-      "@features": resolve(__dirname, "src/features"),
-      "@shared": resolve(__dirname, "src/shared"),
+export default defineConfig(() => {
+  return {
+    plugins: [
+      tanstackRouter({
+        target: "react",
+        autoCodeSplitting: true,
+        routesDirectory: "./src/app/router/routes",
+        generatedRouteTree: "./src/app/router/routeTree.gen.ts",
+        enableRouteTreeFormatting: true, // ! значение по умолчанию. Включает функцию форматирования сгенерированного файла дерева маршрутов, что может занять много времени при работе над крупными проектами
+      }),
+      react(),
+      tailwindcss(),
+      svgr(),
+    ],
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".mjs", ".cjs", ".json"],
+      tsconfigPaths: true,
     },
-  },
+    server: {
+      port: 3000,
+      strictPort: true,
+      open: true,
+      proxy: {},
+    },
+    preview: {
+      port: 2001,
+      strictPort: true,
+      open: true,
+    },
+  };
 });
